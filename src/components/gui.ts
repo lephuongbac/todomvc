6623b064372df2b01c178214a97f1565980e15e1
Bac Le Phuong <rockviet@gmail.com>
2016-11-08 19:06:45 +0700

    update page

--- src/components/gui.ts
+++
@@ -1,5 +0,0 @@
-export { default as button } from './button';
-export { default as input } from './input';
-export { default as link } from './link';
-export { default as label } from './label';
-export { default as checkbox } from './checkbox';
